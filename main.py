4aac98f207830f1b47eb24e6751c685230c1866d
Abdelkader <hamdikader06@gmail.com>
2025-06-18 14:38:28 +0100

    initial creation

---
+++ main.py
@@ -0,0 +1,10 @@
+import streamlit
+
+streamlit.set_page_config(
+    page_title='streamlit toturial' ,
+    page_icon='😶‍🌫️')
+
+streamlit.title("👋 Hello everyone, welcome to our Streamlit session!")
+
+streamlit.sidebar.title("🔧 Navigation")
+streamlit.sidebar.info("Use the sidebar to explore different features.")
